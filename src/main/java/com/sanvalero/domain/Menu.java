7e3d6032bd4f15981f2312fda286f822e06d5184
Martisni <martahusillos1998@gmail.com>
2021-05-20 20:11:44 +0200

    Tercera clase Menu

---
+++ src/main/java/com/sanvalero/domain/Menu.java
@@ -0,0 +1,40 @@
+package com.sanvalero.domain;
+
+public class Menu {
+    private String nombre;
+    private String descripcion;
+    private String precio;
+
+    public Menu() {
+    }
+
+    public Menu(String nombre, String descripcion, String precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+}
